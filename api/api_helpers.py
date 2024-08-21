5e7a22bf65b8ed3dde7beb7ca984a53390e5d820
Алёна Родионова <a.rodionovaa.95@gmail.com>
2024-08-21 21:05:08 +0300

    remove method assert and add allure step annotation

--- api/api_helpers.py
+++ api/api_helpers.py
@@ -1,4 +1,5 @@
 """Модуль содержит методы с генерацией и удалением тестовых данных"""
+import allure
 import requests
 from faker import Faker
 from api.api_urls import Urls
@@ -24,7 +25,7 @@ def first_name_generate():
     return first_name
 
 
-#  удаляем созданного пользователя
+@allure.step('Удаляем только что созданного пользователя')
 def delete_user(response_for_delete_json=None):
     payload_for_delete = {
         "refreshToken": response_for_delete_json['refreshToken']
@@ -32,6 +33,4 @@ def delete_user(response_for_delete_json=None):
     headers_for_delete = {
         "Authorization": response_for_delete_json['accessToken']
     }
-    delete_user_response = requests.delete(Urls.GET_USER_INFO_URL, data = payload_for_delete,
-                                           headers = headers_for_delete)
-    assert delete_user_response.status_code == 202
+    requests.delete(Urls.GET_USER_INFO_URL, data = payload_for_delete, headers = headers_for_delete)
